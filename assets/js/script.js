ecade12a6aaf79237a0b65b6fb413777a7b9069b
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-03 09:04:04 -0400

    task counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");     // querySelector checks for that id in the .document which is the -> html file
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-// IT CREATES A LIST AFTER A CLICK 
+
 
 var taskFormHandler = function(event){                        // It has to be declared before the Listener to be able to call it, and also it will replicate the code below 
     
@@ -9,6 +10,9 @@ var taskFormHandler = function(event){                        // It has to be de
     
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value; 
+
+
+    
 
     if(!taskNameInput || !taskTypeInput) {
         alert("You ned to fill out the task form!")
@@ -31,12 +35,18 @@ var taskFormHandler = function(event){                        // It has to be de
     var listItemE1 = document.createElement('li');       //create a new task item 
     listItemE1.className = "task-item";               // It will give the same CSS style as this class name so the file is uniform   // style new task item
     
+    // add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+
     var taskInfoEl = document.createElement("div"); // create div to hold task info and add to list item
     taskInfoEl.className = "task-info"; // give it a class name
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>"; // add HTML content to div
     
     listItemE1.appendChild(taskInfoEl);
-    tasksToDoE1.appendChild(listItemE1);            // I will add the lisItemE1 at the end of the list          
+    tasksToDoE1.appendChild(listItemE1);            // I will add the lisItemE1 at the end of the list 
+    
+    // Increase task counter for next unique id
+    taskIdCounter ++;
 };
 
 formEl.addEventListener("submit", taskFormHandler);
